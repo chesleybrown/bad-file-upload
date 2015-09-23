bbb175a42040e73798a958f07cca0a4df02ae36a
Chesley Brown <me@chesleybrown.ca>
2015-09-23 15:24:22 -0230

    Added simple page that renders form upload input

--- app/index.js
+++ app/index.js
@@ -17,6 +17,12 @@ var upload = multer({
 	storage: storage
 });
 
+// Show file upload form
+app.get('/', upload.single('image'), function (req, res) {
+	res.send('<form action="./upload" method="post" enctype="multipart/form-data">Upload: <input type="file" name="image" /><input type="submit" value="Submit" /></form>');
+});
+
+// Post file upload to this path
 app.post('/upload', upload.single('image'), function (req, res) {
 	res.status(201).end();
 });
