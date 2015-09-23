27834f2e2a678c45cff9eadef32cdee766780b90
Chesley Brown <me@chesleybrown.ca>
2015-09-23 15:13:37 -0230

    Added support for PORT env

--- app/server.js
+++ app/server.js
@@ -2,9 +2,7 @@
 
 var app = require('./index');
 
-var server = app.listen(3000, function () {
-	var host = server.address().address;
+var server = app.listen(process.env.PORT || 3000, function () {
 	var port = server.address().port;
-
-	console.log('Listening at http://%s:%s', host, port);
+	console.log('Listening on port %s', port);
 });
