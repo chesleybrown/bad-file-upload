824de6777bc26da00bf723d29ab5ec4478d02488
Chesley Brown <me@chesleybrown.ca>
2015-09-23 22:12:01 -0230

    Added dumb image check and really dumb file backup method
    - includes dumb tests

--- app/index.js
+++ app/index.js
@@ -4,17 +4,31 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 var multer  = require('multer');
+var exec = require('child_process').exec;
+var uploadDir = __dirname + '/uploads/';
 
 // Set upload destination and filenaming convention
 var storage = multer.diskStorage({
-	destination: __dirname + '/uploads/',
+	destination: uploadDir,
 	filename: function (req, file, cb) {
 		cb(null, file.originalname);
 	}
 });
 
 var upload = multer({
-	storage: storage
+	storage: storage,
+	fileFilter: function (req, file, cb) {
+		// Only accept image files
+		var acceptable = ['.png', '.jpg', '.gif'];
+		for (var key in acceptable) {
+			if (file.originalname.match(acceptable[key] + '$')) {
+				// Valid extension
+				return cb(null, true);
+			}
+		}
+		// not valid extension
+		cb(null, false);
+	}
 });
 
 // Show file upload form
@@ -24,7 +38,14 @@ app.get('/', upload.single('image'), function (req, res) {
 
 // Post file upload to this path
 app.post('/upload', upload.single('image'), function (req, res) {
-	res.status(201).end();
+	if (!req.file) {
+		return res.status(400).end();
+	}
+
+	// Make a backup of the uploaded file
+	exec('cp ' + uploadDir + req.file.originalname + ' ' + __dirname + '/backups/', function () {
+		res.status(201).end();
+	});
 });
 
 module.exports = app;
--- test/index.spec.js
+++ test/index.spec.js
@@ -8,35 +8,92 @@ var fs = require('fs');
 describe('File Upload', function () {
 	var request;
 	var response;
-	var uploadedFile = __dirname + '/../app/uploads/screenshot.png';
-
-	beforeEach(function () {
-		try {
-			fs.unlinkSync(uploadedFile);
-		}
-		catch (err) {
-
-		}
-	});
 
 	describe('when uploading a file', function () {
-		beforeEach(function (done) {
-			request = requester(app)
-				.post('/upload')
-				.attach('image', __dirname + '/fixtures/screenshot.png')
-				.end(function (err, res) {
-					if (err) return done(err);
-					response = res;
-					done();
-				})
-			;
+		describe('and it is a TXT file', function () {
+			var uploadedFile = __dirname + '/../app/uploads/text.txt';
+			var backedUpFile = __dirname + '/../app/backups/text.txt';
+
+			// clear any uploaded files leftover
+			beforeEach(function () {
+				try {
+					fs.unlinkSync(uploadedFile);
+					fs.unlinkSync(backedUpFile);
+				}
+				catch (err) {
+				}
+			});
+
+			beforeEach(function (done) {
+				request = requester(app)
+					.post('/upload')
+					.attach('image', __dirname + '/fixtures/text.txt')
+					.end(function (err, res) {
+						if (err) return done(err);
+						response = res;
+						done();
+					})
+				;
+			});
+
+			it('should respond with success', function () {
+				expect(response.status).to.equal(400);
+			});
+			it('should NOT write file to disk', function () {
+				var check = function () {
+					fs.accessSync(uploadedFile);
+				};
+				expect(check).to.throw();
+			});
+			it('should NOT backup file to disk', function () {
+				var check = function () {
+					fs.accessSync(uploadedFile);
+				};
+				expect(check).to.throw();
+			});
 		});
 
-		it('should write file to disk', function () {
-			var check = function () {
-				fs.accessSync(uploadedFile);
-			};
-			expect(check).to.not.throw();
+		describe('and it is a PNG', function () {
+			var uploadedFile = __dirname + '/../app/uploads/screenshot.png';
+			var backedUpFile = __dirname + '/../app/backups/screenshot.png';
+
+			// clear any uploaded files leftover
+			beforeEach(function () {
+				try {
+					fs.unlinkSync(uploadedFile);
+					fs.unlinkSync(backedUpFile);
+				}
+				catch (err) {
+				}
+			});
+
+			beforeEach(function (done) {
+				request = requester(app)
+					.post('/upload')
+					.attach('image', __dirname + '/fixtures/screenshot.png')
+					.end(function (err, res) {
+						if (err) return done(err);
+						response = res;
+						done();
+					})
+				;
+			});
+
+			it('should respond with success', function () {
+				expect(response.status).to.equal(201);
+			});
+			it('should write file to disk', function () {
+				var check = function () {
+					fs.accessSync(uploadedFile);
+				};
+				expect(check).to.not.throw();
+			});
+			it('should write backup file to disk', function () {
+				var check = function () {
+					fs.accessSync(backedUpFile);
+				};
+				expect(check).to.not.throw();
+			});
 		});
 	});
 
